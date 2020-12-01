4cbd202a9b6a73e6e0701c572591887ce78f0bfb
Joshua Ukpozi <jukpoz@gmail.com>
2020-12-01 21:52:53 +0100

    seventh commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,11 +1,5 @@
 import 'dart:async';
-
-import 'package:agorartm/resources/repository.dart';
-import 'package:agorartm/screens/google_signIn_screen.dart';
-import 'package:agorartm/screens/insta_home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gradient_text/gradient_text.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -13,8 +7,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  Repository _repository = Repository();
   var image = Image.asset('assets/images/agoraLogo.png');
+  var image2 = Image.asset('assets/images/3333.jpg');
 
   @override
   void initState() {
@@ -26,6 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     precacheImage(image.image, context);
+    precacheImage(image2.image, context);
   }
 
   startTime() async {
@@ -34,21 +29,13 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void navigationPage() {
-    // Navigator.pushReplacement(
-    //   context,
-    //   MaterialPageRoute(
-    //     builder: (context) {
-    //       return LoginScreen();
-    //     },
-    //   ),
-    // );
-
     Navigator.of(context).pushReplacementNamed('/HomeScreen');
   }
 
   @override
   Widget build(BuildContext context) {
     precacheImage(AssetImage("assets/images/agoraLogo.png"), context);
+    precacheImage(AssetImage("assets/images/3333.jpg"), context);
     return Scaffold(
       backgroundColor: Color(0xff252E39), //Color(0xFF2C3F3F),
       body: Container(
@@ -67,36 +54,12 @@ class _SplashScreenState extends State<SplashScreen> {
                     height: 80,
                     width: 80,
                     child: Center(
-                      child: image,
+                      child: image2,
                     ),
                   ),
                 ],
               ),
             ),
-            // Container(
-            //   alignment: Alignment.center,
-            //   child: Column(
-            //     crossAxisAlignment: CrossAxisAlignment.center,
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: <Widget>[
-            //       Row(
-            //         mainAxisAlignment: MainAxisAlignment.center,
-            //         children: <Widget>[
-            //           Text('With ', style: TextStyle(color: Colors.white,fontSize: 13),),
-            //           Icon(Icons.favorite, color: Colors.blue,)
-            //         ],
-            //       ),
-            //       SizedBox(height: 4,),
-            //       GradientText('DEVELOPERS',
-            //           gradient: LinearGradient(
-            //               colors: [Colors.lightBlueAccent,  Colors.blue, Colors.indigo]),
-            //           style: TextStyle(fontSize: 16,),
-            //           textAlign: TextAlign.center,
-            //       ),
-
-            //     ],
-            //   ),
-            // )
           ],
         ),
       ),
